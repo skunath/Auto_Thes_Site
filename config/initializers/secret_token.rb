0041e49252e718d6e83f477d2cbcc1ded781a52e
S. Kunath <skunath@gmail.com>
2012-01-21 12:45:23 -0500

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AutoThesSite::Application.config.secret_token = '4ee2834950f91c91bb04d9f7f0a085cde5f5638d022226cccad435ef016a1274e5df062347786134015871b4a71cc96bb48aeb9e4e16930590b765c0e235b6e6'
